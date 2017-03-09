a3c130756a401f49eaac61087f3f43d0f66963ca
Haochen Zhao <haochen@huancheclub.com>
2017-03-09 09:56:49 +0800

    change interpolate delimiter

--- slushfile.js
+++ slushfile.js
@@ -87,7 +87,7 @@ gulp.task('default', function (done) {
             }
             answers.appNameSlug = _.slugify(answers.appName);
             gulp.src(__dirname + '/templates/**/*')
-                .pipe(template(answers, {interpolate: '/{{([\s\S]+?)}}/g'}))
+                .pipe(template(answers, {interpolate: /<%=([\s\S]+?)%>/g}))
                 .pipe(rename(function (file) {
                     if (file.basename[0] === '_') {
                         file.basename = '.' + file.basename.slice(1);
