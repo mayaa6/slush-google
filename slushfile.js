46f78d7f66f70f1da38cb2cdba7e9d0426660573
Haochen Zhao <haochen@huancheclub.com>
2017-03-09 10:07:22 +0800

    remove prompts, leave only one

--- slushfile.js
+++ slushfile.js
@@ -51,33 +51,10 @@ var defaults = (function () {
 })();
 
 gulp.task('default', function (done) {
-    var prompts = [{
-        name: 'appName',
-        message: 'What is the name of your project?',
-        default: defaults.appName
-    }, {
-        name: 'appDescription',
-        message: 'What is the description?'
-    }, {
-        name: 'appVersion',
-        message: 'What is the version of your project?',
-        default: '0.1.0'
-    }, {
-        name: 'authorName',
-        message: 'What is the author name?',
-        default: defaults.authorName
-    }, {
-        name: 'authorEmail',
-        message: 'What is the author email?',
-        default: defaults.authorEmail
-    }, {
-        name: 'userName',
-        message: 'What is the github username?',
-        default: defaults.userName
-    }, {
+    var prompts = [ {
         type: 'confirm',
         name: 'moveon',
-        message: 'Continue?'
+        message: 'Scaffold a lastest Google Web Starter Kit(0.6.5)?'
     }];
     //Ask
     inquirer.prompt(prompts,
